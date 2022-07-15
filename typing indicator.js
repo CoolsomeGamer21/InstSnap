f3a982fc318b07695752d874f6de849ad0f222d1
CoolsomeGamer21 <105989804+CoolsomeGamer21@users.noreply.github.com>
2022-07-15 15:23:01 -0400

    Update typing indicator.js

--- typing indicator.js
+++ typing indicator.js
@@ -1,4 +1,5 @@
 JavaScript default 
 intercept the keydown event inputBox.on('keydown', function(e) { 
-// if the RETURN/ENTER key is pressed, send the message if (e.keyCode === 13) 
-{ sendButton.click(); } else { // else send the Typing Indicator signal activeChannel.typing(); } });
+if the RETURN/ENTER key is pressed, send the message if (e.keyCode === 13) 
+{ sendButton.click(); 
+} else { else send the Typing Indicator signal activeChannel.typing(); } });
